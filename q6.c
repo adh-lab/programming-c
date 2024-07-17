a083e576ab5d5ad4ba116712828a3a499149542f
Adharsh babu <52247192+adh-lab@users.noreply.github.com>
2024-07-17 14:44:09 +0530

    Add files via upload

---
+++ q6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    int x,y,z;
+    float k;
+    printf("\n enter numbers");
+    scanf("%d\n%d\n%d",&x,&y,&z);
+    k=x+y-x=y;
+    printf("\n d=%.3f,x=%d",k,y);
+    return 0;
+}
