4d58c2a25fc03ef4754f7b7474858e9ff47b629e
Adharsh babu <52247192+adh-lab@users.noreply.github.com>
2024-07-18 10:16:44 +0530

    Add files via upload

---
+++ lo2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k;
+printf("\n enter values of i and j");
+scanf("%d\n%d",&i,&j);
+k=i||j;
+printf("\nk=%d",k);
+return 0;
+}
---
+++ lo3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int i,k;
+printf("\n enter values of i");
+scanf("%d",&i);
+k=!i;
+printf("\nk=%d",k);
+return 0;
+}
